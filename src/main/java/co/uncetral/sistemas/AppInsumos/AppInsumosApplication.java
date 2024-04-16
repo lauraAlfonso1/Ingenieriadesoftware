7bc730bd7e86f19250c286c83f1c1fe8b423bb77
omarda-04 <omarda1406@gmail.com>
2024-04-16 18:57:13 -0500

    Develop: commit organizando proyecto.

---
+++ src/main/java/co/uncetral/sistemas/AppInsumos/AppInsumosApplication.java
@@ -0,0 +1,13 @@
+package co.uncetral.sistemas.AppInsumos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInsumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInsumosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uncetral/sistemas/AppInsumos/AppInsumosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uncetral.sistemas.AppInsumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInsumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
