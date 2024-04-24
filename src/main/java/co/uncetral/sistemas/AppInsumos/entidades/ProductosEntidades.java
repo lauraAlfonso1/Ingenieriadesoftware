864d5d413e478d890925be94091716073f66fce2
omarda-04 <omarda1406@gmail.com>
2024-04-23 23:27:51 -0500

    Se agrego la entidad con su creacion a la base de datos y se configuro el pom.xml ProductosEntidades

--- src/main/java/co/uncetral/sistemas/AppInsumos/entidades/ProductosEntidades.java
+++ src/main/java/co/uncetral/sistemas/AppInsumos/entidades/ProductosEntidades.java
@@ -1,4 +1,36 @@
-package Ingenieriadesoftware.src.main.java.co.uncetral.sistemas.AppInsumos.entidades;
+package co.uncetral.sistemas.AppInsumos.entidades;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
 
+
+import java.io.Serializable;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "Productos")
 public class ProductosEntidades {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PRODUCTOS")
+    @SequenceGenerator(name = "SEQ_PRODUCTOS",sequenceName = "SEQ_PRODUCTOS", allocationSize = 1)
+    @Column(name = "PROD_CODIGO")
+    private long codigo;
+
+    @Column(name = "PROD_MARCA")
+    private String nombre;
+
+    @Column(name = "PROD_MODELO")
+    private String modelo;
+
+    @Column(name = "PRO_DESCRIPCION")
+    private String descripcion;
+
+    @Column(name = "PROD_CANTIDAD")
+    private int cantidad;
+
+
+
+
 }
