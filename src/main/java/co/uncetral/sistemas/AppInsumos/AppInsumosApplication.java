3e69369e9ad1135fb8b6309055433d3ff63b91d9
omarda-04 <omarda1406@gmail.com>
2024-05-04 20:14:50 -0500

    Se actualiza y se termina CU-02: Registrar ProductosEntidades

--- src/main/java/co/uncetral/sistemas/AppInsumos/AppInsumosApplication.java
+++ src/main/java/co/uncetral/sistemas/AppInsumos/AppInsumosApplication.java
@@ -8,22 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class AppInsumosApplication implements CommandLineRunner{
+public class AppInsumosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppInsumosApplication.class, args);
 	}
 
-	@Autowired
-	private RepositorioProductos repo;
 
-	@Override
-	public void run(String... args) throws Exception {
-		ProductosEntidades pro1 =new ProductosEntidades(12,"p1","hy23","compuxd",4);
-		repo.save(pro1);
 
-		ProductosEntidades pro2 =new ProductosEntidades(34,"p2","lk45","celuxd",2);
-		repo.save(pro2);
-
-	}
 }
